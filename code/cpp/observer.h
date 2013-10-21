b400684de7591509885e743abb6098c356283934
Bob Nystrom <robert@stuffwithstuff.com>
2013-10-20 21:48:13 -0700

    Stash context param notes.

---
+++ code/cpp/observer.h
@@ -0,0 +1,14 @@
+//
+//  observer.h
+//  cpp
+//
+//  Created by Bob Nystrom on 10/19/13.
+//  Copyright (c) 2013 Bob Nystrom. All rights reserved.
+//
+
+#ifndef cpp_observer_h
+#define cpp_observer_h
+
+
+
+#endif
